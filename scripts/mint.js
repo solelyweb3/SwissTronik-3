3e22b9e7bfe32ebebeb6b9c27148ee817c7d1462
solelyweb3 <root@vm2717990.stark-industries.solutions>
2024-07-30 10:22:14 +0000

    Initial Commit

---
+++ scripts/mint.js
@@ -0,0 +1,36 @@
+const hre = require("hardhat");
+
+const { encryptDataField, decryptNodeResponse } = require("@swisstronik/utils");
+
+const sendShieldedTransaction = async (signer, destination, data, value) => {
+  const rpcLink = hre.network.config.url;
+  const [encryptedData] = await encryptDataField(rpcLink, data);
+  return await signer.sendTransaction({
+    from: signer.address,
+    to: destination,
+    data: encryptedData,
+    value,
+  });
+};
+
+async function main() {
+  const contractAddress = "0x19A347d50460AcD0D2Be5a9459506b0f42BaEA62"; // nft contract address
+  const [signer] = await hre.ethers.getSigners();
+  const contractFactory = await hre.ethers.getContractFactory("solelyweb3");
+  const contract = contractFactory.attach(contractAddress);
+  const functionName = "safeMint";
+  const safeMintTx = await sendShieldedTransaction(
+    signer,
+    contractAddress,
+    contract.interface.encodeFunctionData(functionName, [signer.address, 1]),
+    0
+  );
+  await safeMintTx.wait();
+  console.log('safeMintTx hash: ', safeMintTx.hash);
+  console.log(`Transaction URL of Mint: https://explorer-evm.testnet.swisstronik.com/tx/${safeMintTx.hash}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
